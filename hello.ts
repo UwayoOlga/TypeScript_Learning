ffcd9b00740926f6cd687dc1d83f919ec563775c
UwayoOlga <uwayoolga@gmail.com>
2025-06-14 19:00:31 +0200

    initial commit

--- hello.ts
+++ hello.ts
@@ -4,3 +4,12 @@ function greet(name: string): string {
 
 const userName = "Olga";
 console.log(greet(userName));
+
+let value: unknown;
+
+value = 10;         // OK
+value = "test";      
+
+if (typeof value === "string") {
+  console.log(value.toUpperCase());  // OK after type check
+}
